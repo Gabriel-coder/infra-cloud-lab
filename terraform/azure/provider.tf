820d71f3fcd6905ac56f2d96035d4c77ef611476
Rodrigo Mello <rodrigospinola3@hotmail.com>
2025-07-12 15:54:30 -0300

    fix: usando autenticação via ambiente ARM_* no provider

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -1,3 +1,15 @@
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+}
