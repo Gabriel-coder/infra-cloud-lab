8691aaed1e8095f2d146ed5b42865642ded6f12a
Rodrigo Mello <rodrigospinola3@hotmail.com>
2025-07-12 15:53:08 -0300

    removendo auth manual do provider.tf para usar login do GitHub Actions

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -1,21 +1,3 @@
 provider "azurerm" {
   features {}
-
-  tenant_id       = jsondecode(var.azure_credentials).tenantId
-  subscription_id = jsondecode(var.azure_credentials).subscriptionId
-  client_id       = jsondecode(var.azure_credentials).clientId
-  client_secret   = jsondecode(var.azure_credentials).clientSecret
-}
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-
-    random = {
-      source = "hashicorp/random"
-    }
-  }
 }
